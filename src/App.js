f96be7b14b62953d536a4f26011f17d5f23d898b
omar <baiomayomar@gmail.com>
2024-10-02 12:04:50 +0300

    add search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import {FaAccusoft} from "react-icons/fa6"
+/**
+ * Components
+ */
+
+import Search from "./components/Search"
+
+
+/**
+ * React Icons
+ */
+import {FaCalendar} from "react-icons/fa6"
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <>
+    <h1 className="text-3xl font-thin text-center py-10">
+    <FaCalendar className="inline"/> My Appointments 
     </h1>
+    <Search/>
+    </>
+    
   )
 }
