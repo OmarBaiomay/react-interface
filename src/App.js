c729b7a1d09089ca6b02a3ef06aff3037aa604e2
omar <baiomayomar@gmail.com>
2024-10-02 15:06:00 +0300

    search and add appointment comps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+
+import { useState ,useCallback, useEffect} from "react"
+
+
 /**
  * Components
  */
 
 import Search from "./components/Search"
-
+import AddAppointment from "./components/AddAppointment"
+import AppointmentInfo from "./components/AppointmentInfo"
 
 /**
  * React Icons
  */
-import {FaCalendar} from "react-icons/fa6"
+import {BiCalendar} from "react-icons/bi"
 
 export default function App() {
+
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  let fetchData = useCallback(
+    () => {
+      fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)
+      })
+    },
+    [],
+  )
+
+  useEffect(()=>{
+    fetchData()
+  }, [fetchData])
+  
+
   return (
-    <>
+    <div className="container">
     <h1 className="text-3xl font-thin text-center py-10">
-    <FaCalendar className="inline"/> My Appointments 
+    <BiCalendar className="inline"/> My Appointments 
     </h1>
     <Search/>
-    </>
+
+    <AddAppointment />
+
+    <ul>
+      {appointmentList.map(appointment => (
+        <AppointmentInfo appointment={appointment} key={appointment.id}/>
+      ))}
+      
+    </ul>
+    </div>
     
   )
 }
